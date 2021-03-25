5cfe22fff5ae5443426d31518999bc17c87e38f5
Antti Köliö <antti.kolio@gmail.com>
2021-03-25 12:48:07 +0200

    url protocol base

--- Sources/MegicalEasyAccess-SDK-iOS/Http/EARedirectURLProtocolBase.swift
+++ Sources/MegicalEasyAccess-SDK-iOS/Http/EARedirectURLProtocolBase.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  EARedirectURLProtocolBase.swift
 //  
 //
 //  Created by Antti Köliö on 23.2.2021.
@@ -8,116 +8,136 @@
 import Foundation
 import SwiftyBeaver
 
-@objc
-public class EARedirectURLProtocolBase: URLProtocol {
-    
-    let log = SwiftyBeaver.self
+open class EARedirectURLProtocolBase: URLProtocol {
     
     /**
      Override to define callback url
      */
-    @objc open class func oauthCallback() -> String {
+    open class func oauthCallback() -> String {
         return "com.example.app:/oauth-callback"
     }
-    
+
     /**
      Override to define notification name
      */
-    @objc open class func tokenReceivedNotificationName() -> String {
-        return "easyAccessTokenReceived"
+    open class func authCodeReceivedNotificationName() -> String {
+        return "easyAccessAuthCodeReceived"
     }
-    
+
     /**
-     Override if needed. Default ops:
-     #swift
-     let notification = Notification(name: Notification.Name(notificationName), object: notificationDict)
-     NotificationCenter.default.post(notification)
-     
-     #objc
-     NSNotification *notification = [NSNotification notificationWithName:notificationName object:notificationDict];
-     [[NSNotificationCenter defaultCenter] postNotification:notification];
+     Override if needed
      */
-    @objc public class func authCallbackAction() -> ((_ notificationName: String, _ notificationDict: [String: Any]) -> Void) {
+    open class func authCallbackAction() -> ((_ notificationName: String, _ notificationDict: [String: Any]) -> Void) {
         return { (notificationName: String, notificationDict: [String: Any]) in
-            let notification = Notification(name: Notification.Name(notificationName), object: notificationDict)
+            let notification = Notification(name: .init(notificationName), object: notificationDict)
             NotificationCenter.default.post(notification)
         }
     }
-    
+
     /**
-     Override with something like:
-      return [request.URL.scheme isEqualToString:@"com.example.app"];
+     Override if needed
      */
-    @objc public override class func canInit(with request: URLRequest) -> Bool {
-        return false
+    open override class func canInit(with request: URLRequest) -> Bool {
+        guard let requestURL = request.url else {
+            return false
+        }
+        
+        let oauthCallback = Self.oauthCallback()
+        guard let callbackComponents = URLComponents(string: oauthCallback) else {
+            return false
+        }
+        
+        guard callbackComponents.scheme == requestURL.scheme else {
+            return false
+        }
+
+        guard callbackComponents.path == requestURL.path else {
+            return false
+        }
+
+        return true
     }
     
-    @objc public override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+    open override class func canonicalRequest(for request: URLRequest) -> URLRequest {
         return request
     }
-    
-    @objc public override class func requestIsCacheEquivalent(_ a: URLRequest, to b: URLRequest) -> Bool {
+
+    open override class func requestIsCacheEquivalent(_ a: URLRequest, to b: URLRequest) -> Bool {
         return false
     }
-    
-    @objc public override func startLoading() {
-        log.info("EARedirectURLProtocolBase.startLoading \(self.request)")
-        
-        guard let url = self.request.url else {
+
+    //components.host: authcallback
+    //components.path:
+    //query item code: I78CcA7qVMw5ECaG4naRNXhqbGnEdB-2Oi1UK35iDX8.5srB2lUU_RzzaF1wfMgSV4LkS_cfrHH79EEygIITQps
+    //query item scope: openid
+    //query item state: 1870EA3D-185D-435C-8E77-F795F58BACAA
+    open override func startLoading() {
+        guard let requestURL = self.request.url else {
+            return
+        }
+        guard let components = URLComponents(url: requestURL, resolvingAgainstBaseURL: true) else {
             return
         }
         
-        guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
-            return
-        }
-        
+        // already checked in canInitWithRequest
         let oauthCallback = Self.oauthCallback()
         guard let callbackComponents = URLComponents(string: oauthCallback) else {
             return
         }
         
-        guard urlComponents.host == callbackComponents.host else {
-            return
-        }
-        
-        guard urlComponents.path == callbackComponents.path else {
-            return
-        }
-        
-        guard let queryItems = urlComponents.queryItems else {
-            return
-        }
-        
-        var code = ""
-        var scope = ""
-        var state = ""
-        
-        for qi: URLQueryItem in queryItems {
-            if qi.name == "code" {
-                code = qi.value ?? ""
-            } else if qi.name == "scope" {
-                scope = qi.value ?? ""
-            } else if qi.name == "state" {
-                state = qi.value ?? ""
+        if components.path == callbackComponents.path {
+            
+            var code: String?
+            var scope: String?
+            var state: String?
+            
+            if let queryItems = components.queryItems {
+                for qi: URLQueryItem in queryItems {
+                    switch qi.name {
+                    case "code":
+                        code = qi.value
+                    case "scope":
+                        scope = qi.value
+                    case "state":
+                        state = qi.value
+                    default:
+                        break
+                    }
+                }
+                
+                if (code != nil && scope != nil && state != nil) {
+                    
+                    let notificationDict = [
+                        "code": code!,
+                        "scope": scope!,
+                        "state": state!
+                    ] as [String: Any]
+                    
+                    let notificationName = Self.authCodeReceivedNotificationName()
+                    Self.authCallbackAction()(notificationName, notificationDict)
+                    
+                    
+                    
+                    if let urlResponse = HTTPURLResponse(url: requestURL,
+                                                         statusCode: 200,
+                                                         httpVersion: "HTTP/1.1",
+                                                         headerFields: nil),
+                       let client = self.client {
+                        
+                        client.urlProtocol(self,
+                                           didReceive: urlResponse,
+                                           cacheStoragePolicy: .notAllowed)
+                        
+                        client.urlProtocolDidFinishLoading(self)
+                    }
+                    
+                }
             }
+            
         }
-        
-        log.info("code \(code)\nscope \(scope)\nstate \(state)")
-        
-        let notificationDict = ["code": code,
-                                "scope": scope,
-                                "state": state]
-        let notificationName = Self.tokenReceivedNotificationName()
-        let authCallbackAction = Self.authCallbackAction()
-        authCallbackAction(notificationName, notificationDict)
-        
-        if let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: nil) {
-            self.client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-        }
-        self.client?.urlProtocolDidFinishLoading(self)
     }
-        
-    @objc public override func stopLoading() {
+
+    open override func stopLoading() {
         
     }
 }
